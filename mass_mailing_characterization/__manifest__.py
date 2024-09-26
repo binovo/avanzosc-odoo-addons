2bdc43ce92bf41043c7f426e478d7d173fcb0ddd
marras <marras@binovo.es>
2024-09-26 10:43:45 +0200

    [UPD][15317] Modificada vista y traducción

--- mass_mailing_characterization/__manifest__.py
+++ mass_mailing_characterization/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Mass Mailing Characterization",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Marketing",
     "license": "AGPL-3",
     "author": "Trey (www.trey.es)",
